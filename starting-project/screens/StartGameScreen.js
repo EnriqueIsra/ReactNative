dd7f6208c8ee1b3586fbcd8d965033d4a08d1499
EnriqueIsra <sirilo99x3@gmail.com>
2025-09-25 20:49:22 -0600

    50. Syling for Android & iOS

--- starting-project/screens/StartGameScreen.js
+++ starting-project/screens/StartGameScreen.js
@@ -1,9 +1,9 @@
-import { TextInput, View } from "react-native";
+import { TextInput, View, StyleSheet } from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
 
 function StartGameScreen() {
   return (
-    <View>
+    <View style={styles.inputContainer} >
       <TextInput />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
@@ -12,3 +12,21 @@ function StartGameScreen() {
 }
 
 export default StartGameScreen;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    // flex: 1,
+    marginTop: 100,
+    padding: 16,
+    backgroundColor: "#72063c",
+    marginHorizontal: 24,
+    borderRadius: 8,
+    // sombra para Android con elevation
+    elevation: 4,
+    // sombra para iOS con shadow
+    shadowColor: "black",
+    shadowOffset: {width: 0, height: 2},
+    shadowRadius: 6,
+    shadowOpacity: 0.25
+  }
+})
